b877bcd2b0d1dec2b04ef1ae08b79fe765f74084
altbrace <dim203286@gmail.com>
2020-04-22 20:12:22 +0700

    Class Button implemented, added 'play' and 'exit' buttons, their functions defined

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 
-TEXT_COLOR = (41, 55, 247)
+
+TEXT_COLOR = (90, 100, 252)
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -12,12 +13,74 @@ clock = pygame.time.Clock()
 background_image = pygame.image.load('images/background.png')
 pygame.font.init()
 font = pygame.font.Font('misc/font.otf', 40)
+line_size = font.get_linesize()
+screen.blit(background_image, (0, 0))
+
+
+class Button:
+
+    def __init__(self):
+        self.playButton_presence = False
+        self.exitButton_presence = False
+
+    def playButton(self, event):
+        size = font.size('Играть')
+        width, height = size
+        x_position = 400 - width / 2
+        y_position = 200
+        position = (x_position, y_position)
+        if not self.playButton_presence:
+            text = font.render('Играть', True, TEXT_COLOR)
+            screen.blit(text, position)
+            self.playButton_presence = True
+        x, y = pygame.mouse.get_pos()
+        if (x_position <= x <= x_position + width) and (
+                y_position <= y <= y_position + height) and event.type == pygame.MOUSEBUTTONDOWN:
+            return True
+
+    def exitButton(self, event):
+        size = font.size('Выход')
+        width, height = size
+        x_position = 400 - width / 2
+        y_position = 200 + line_size * 2
+        position = (x_position, y_position)
+        if not self.exitButton_presence:
+            text = font.render('Выход', True, TEXT_COLOR)
+            screen.blit(text, position)
+            self.exitButton_presence = True
+        x, y = pygame.mouse.get_pos()
+        if (x_position <= x <= x_position + width) and (
+                y_position <= y <= y_position + height) and event.type == pygame.MOUSEBUTTONDOWN:
+            return True
+
+
+def pressed(btn: Button):
+    pass
+
+
+def game():
+    game_over = False
+
+    if not game_over:
+        screen.blit(background_image, (0, 0))
+        text = font.render('Игра началась', True, TEXT_COLOR)
+        screen.blit(text, (0, 0))
+        pygame.display.update()
 
 
 while True:
-    screen.blit(background_image, (0, 0))
-    pygame.display.update()
-    clock.tick(60)
     for event in pygame.event.get():
+        button = Button()
+        screen.blit(background_image, (0, 0))
+
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if button.playButton(event):
+            game()
+
+        if button.exitButton(event):
+            sys.exit()
+
+        pygame.display.update()
+        clock.tick(60)
