07fe472b44929eef24cbe27a35a925a1399aa10b
Nurvive <jiimbo689@gmail.com>
2020-04-22 22:50:24 +0700

    add randomword function and page with alphabet

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import sys
-
+from time import sleep
+import random
 
 TEXT_COLOR = (90, 100, 252)
 
 pygame.init()
+alphabet = 'а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я'.split()
+words = 'муха кошка пиво лампа шершень лимон хорёк вымпел гроза курица чукча таблица царство мощь носки ножницы арбуз пингвин'.split()
 screen = pygame.display.set_mode((800, 600))
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
@@ -15,6 +18,10 @@ pygame.font.init()
 font = pygame.font.Font('misc/font.otf', 40)
 line_size = font.get_linesize()
 screen.blit(background_image, (0, 0))
+size = font.size('Выход')
+width, height = size
+missedletters = ''
+correctletters = ''
 
 
 class Button:
@@ -24,8 +31,6 @@ class Button:
         self.exitButton_presence = False
 
     def playButton(self, event):
-        size = font.size('Играть')
-        width, height = size
         x_position = 400 - width / 2
         y_position = 200
         position = (x_position, y_position)
@@ -38,11 +43,7 @@ class Button:
                 y_position <= y <= y_position + height) and event.type == pygame.MOUSEBUTTONDOWN:
             return True
 
-    def exitButton(self, event):
-        size = font.size('Выход')
-        width, height = size
-        x_position = 400 - width / 2
-        y_position = 200 + line_size * 2
+    def exitButton(self, event, x_position=400 - width / 2, y_position=200 + line_size * 2):
         position = (x_position, y_position)
         if not self.exitButton_presence:
             text = font.render('Выход', True, TEXT_COLOR)
@@ -56,37 +57,71 @@ class Button:
         # TODO: реализовать обведение/подчеркивание кнопок при наведении курсора
 
 
+
 def pressed(btn: Button):
     pass
+
+
 # TODO: нажатие клавиши = MOUSEBUTTONDOWN + MOUSEBUTTONUP
 
 
 def game():
-    game_over = False
+    game = True
+    secretword = getRandomWord(words)
+    screen.blit(background_image, (0, 0))
+    k = 450
+    e = 70
+    for i in range(len(alphabet)):
+        text = font.render(alphabet[i], True, TEXT_COLOR)
+        screen.blit(text, (k, e))
+        k += 50
+        if i % 9 == 0 and i != 0:
+            k = 450
+            e += 65
+    text = font.render(secretword, True, TEXT_COLOR)
+    screen.blit(text, (700 // 2, 30))
+    pygame.display.update()
+    while game:
 
-    if not game_over:
-        screen.blit(background_image, (0, 0))
-        text = font.render('Игра началась', True, TEXT_COLOR)
-        screen.blit(text, (0, 0))
+        clock.tick(60)
+        for event in pygame.event.get():
+            button = Button()
+            if event.type == pygame.QUIT:
+                exit()
+            if button.exitButton(event, x_position=650 - width / 2, y_position=350 + line_size * 2):
+                main_menu()
+
+
+
         pygame.display.update()
-
     # TODO: написать Виселицу. В левой части окна отрисовка картинки виселицы,
     #  в правой - массив кнопок букв. Сверху слово
 
 
-while True:
-    for event in pygame.event.get():
-        button = Button()
-        screen.blit(background_image, (0, 0))
+def getRandomWord(wordlist):
+    wordindex = random.randint(0, len(wordlist) - 1)
+    return wordlist[wordindex]
 
-        if event.type == pygame.QUIT:
-            sys.exit()
 
-        if button.playButton(event):
-            game()
+def main_menu():
+    while True:
+        for event in pygame.event.get():
+            button = Button()
+            screen.blit(background_image, (0, 0))
 
-        if button.exitButton(event):
-            sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-        pygame.display.update()
-        clock.tick(60)
+            if button.playButton(event):
+                game()
+
+            if button.exitButton(event):
+                sys.exit()
+
+            pygame.display.update()
+            clock.tick(60)
+
+
+if __name__ == '__main__':
+    secretword = getRandomWord(words)
+    main_menu()
