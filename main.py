737533c7af76bd68acf22f2498e4cb23f5f948bc
Nurvive <jiimbo689@gmail.com>
2020-04-23 15:23:56 +0700

    tie objects to size of window

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ TEXT_COLOR = (90, 100, 252)
 
 pygame.init()
 alphabet = 'а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я'.split()
-words = 'муха кошка пиво лампа шершень лимон хорёк вымпел гроза курица чукча таблица царство мощь носки ножницы арбуз ' \
-        'пингвин'.split()
-screen = pygame.display.set_mode((800, 600))
+words = 'пингвин'.split()
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Виселица')
@@ -77,7 +78,7 @@ class Button:
         self.exitButton_presence = False
 
     def play_button(self, event):
-        x_position = 400 - width / 2
+        x_position = SCREEN_WIDTH // 2 - width // 2
         y_position = 200
         position = (x_position, y_position)
         if not self.playButton_presence:
@@ -89,7 +90,7 @@ class Button:
                 y_position <= y <= y_position + height) and event.type == pygame.MOUSEBUTTONDOWN:
             return True
 
-    def exit_button(self, event, x_position=400 - width / 2, y_position=200 + line_size * 2):
+    def exit_button(self, event, x_position=SCREEN_WIDTH // 2 - width // 2, y_position=200 + line_size * 2):
         position = (x_position, y_position)
         if not self.exitButton_presence:
             text = font.render('Выход', True, TEXT_COLOR)
@@ -110,8 +111,8 @@ def pressed(btn: Button):
 # TODO: нажатие клавиши = MOUSEBUTTONDOWN + MOUSEBUTTONUP
 
 def drawing_alphabet():
-    k = 450
-    e = 120
+    k = SCREEN_WIDTH // 1.6
+    e = SCREEN_HEIGHT //6
     global letter_position
     letter_position = []
     for i in range(len(alphabet)):
@@ -119,8 +120,8 @@ def drawing_alphabet():
         screen.blit(text, (k, e))
         letter_position.append((k, e))
         k += 50
-        if i % 9 == 0 and i != 0:
-            k = 450
+        if i % 8 == 0 and i != 0 :
+            k = SCREEN_WIDTH // 1.6
             e += 65
     return letter_position
 
@@ -139,18 +140,17 @@ def check_letter(letter):
 
         if foundallletters:
             alert('ты угадал - ' + secretword)
-        return correct_letters
+        # return correct_letters
 
     else:
         missed_letters = missed_letters + letter
         crossing_letter(letter)
         if len(missed_letters) == (trys - 1):
             alert('не угадал, слово было - ' + secretword)
-        return missed_letters
+        # return missed_letters
 
 
 def crossing_letter(letter):
-
     for i in (missed_letters + correct_letters):
         if i not in already_crossing_letters:
             x1 = letter_position[alphabet.index(i)][0]
@@ -160,17 +160,19 @@ def crossing_letter(letter):
             pygame.draw.aaline(screen, TEXT_COLOR, (x1, y1), (x2, y2))
             already_crossing_letters.append(letter)
 
+
 def display_secret_word():
     blanks = '_' * len(secretword)
     for i in range(len(secretword)):
         if secretword[i] in correct_letters:
             blanks = blanks[:i] + secretword[i] + blanks[i + 1:]
-    k = 0
+    k = SCREEN_WIDTH // 2.5
     for letter in blanks:
         blank = font.render(letter, 1, TEXT_COLOR)
-        screen.blit(blank, (300 + k, 20))
+        screen.blit(blank, (k, 20))
         k += 30
-    pygame.display.update()
+        pygame.display.update()
+
 
 def game():
     game = True
@@ -187,7 +189,8 @@ def game():
             button = Button()
             if event.type == pygame.QUIT:
                 exit()
-            if button.exit_button(event, x_position=650 - width / 2, y_position=350 + line_size * 2):
+            if button.exit_button(event, x_position=SCREEN_WIDTH // 2 + SCREEN_WIDTH // 3.5 - width // 2,
+                                  y_position=SCREEN_HEIGHT // 1.5 + line_size * 2):
                 main_menu()
             if event.type == pygame.KEYDOWN:
                 guess = get_guess(missed_letters + correct_letters, event)
@@ -210,6 +213,7 @@ def alert(text):
     drawing_alphabet()
     display_secret_word()
     pygame.display.update()
+
 
 def get_guess(alreadyguessed, event):
     while True:
